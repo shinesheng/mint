5dd60555dc2ef37486b14def59fcda82a1f557a6
shinesheng <798650453@qq.com>
2019-03-28 14:20:45 +0800

    vue的mint移动端框架尝试

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <mt-button @click.native="handleClick">按钮</mt-button>
+  </div>
+</template>
+
+<script>
+export default{
+  el: '#app',
+  methods: {
+    handleClick: function () {
+      this.$toast('Hello world!')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
